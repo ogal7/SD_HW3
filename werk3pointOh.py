e880310a94350682c73916fd97bbcf0fc09e3bf2
ogal7 <ogallager@stuy.edu>
2016-09-23 17:05:34 -0500

    ugh

--- werk3pointOh.py
+++ werk3pointOh.py
@@ -1,24 +1,24 @@
-#!/usr/bin/python
-#OG
-#Softdev homewerk numero thres
-
-
 from flask import Flask, render_template
 import csv
 import random
 
-
 app = Flask(__name__)
-@app.route('/occupations')
-
-d = {}
-randoJob = ""
 
 
+#root
+@app.route("/")
+def hola():
+    return "how do you solve a problem like Olivia?"
 
-# init a dictionary of occupations, send it to template
+@app.route("/occupations")
+
+def helpMeFindAJob():
+    return render_template('jobs.html', dict = di(), job = ArandoJob())
+
+
 with open('occupations.csv') as csvfile:
     reader = csv.reader(csvfile)
+    d = {}
     for row in reader:
         if (row[0] != "Job Class" and row[0] != "Total"):
             d[row[0]] = row[1]
@@ -29,19 +29,18 @@ li = []
 for x in d.keys():
         for i in range( int(float(d[x]) * 10)):
                 li.append(x)
-
-
-
+#pick a random job to print at the bottom of the table
 randoJob = random.choice(li)
 
+#testing template
+#def helpMeFindAJob():
+ #   return render_template('jobs.html', dict = d, job = randoJob)
 
+def ArandoJob():
+    return randoJob
 
-
-def helpMeFindAJob():
-    return render_template('jobs.html', dict = d, job = randoJob)
-
-
-
+def di():
+    return d
 
 
 if __name__ == '__main__':
